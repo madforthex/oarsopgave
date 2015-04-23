d84c3a9f40793583fb9720f51e061be81c7b223a
ast-it <astit445@gmail.com@outlook.com>
2015-04-23 20:24:25 +0200

    lol
    lol

--- login.py
+++ login.py
@@ -13,24 +13,12 @@ def footer():
     print "</BODY></HTML>"
 
 form = cgi.FieldStorage()
-#username = query.execute("SELECT * FROM `schema` WHERE `username`='" + username + "'")
-password = query.execute("SELECT * FROM `schema` WHERE `username`='" + password + "'")
+username = query.execute("SELECT * FROM `schema` WHERE `username`='" + username + "'")
+password = query.execute("SELECT * FROM `schema` WHERE `password`='" + password + "'")
 
 if not form:
     header("Login Response")
-elif form.has_key("login") and form["login"].value != "" and form.has_key("username") and form["username"].value == username:
-    header("Connected ...")
-    print "<center><hr><H3>Welcome back," , form["login"].value, ".</H3><hr></center>"
-    print r"""<form><input type="hidden" name="session" value="%s"></form>""" % (form["login"].value)
-    print "<H3><a href=http://ast-it.dk>Click here to start browsing</a></H3>"
-
-else:
-    header("No success!")
-    print "<H3>Please go back and enter a valid login.</H3>"
-
-if not form:
-    header("Login Response")
-elif form.has_key("login") and form["login"].value != "" and form.has_key("password") and form["password"].value == password:
+elif form.has_key("login") and form["login"].value != "" and form.has_key("username") and form["username"].value == username && form.has_key("login") and form["login"].value != "" and form.has_key("password") and form["password"].value == password:
     header("Connected ...")
     print "<center><hr><H3>Welcome back," , form["login"].value, ".</H3><hr></center>"
     print r"""<form><input type="hidden" name="session" value="%s"></form>""" % (form["login"].value)
